dc0bc214363ac5b8a18df77f3a211106d297525f
kimg00n <kevin1226716@gmail.com>
2022-09-30 17:59:35 +0900

    little change

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import sys
 import pickle
 from qiling import *
 from qiling.os.uefi.const import *
+from qiling.const import QL_VERBOSE
 
 def run(args):
-    with open(args.nvram_file, "rb") as nv:
-        env = pickle.load(nv)
+    print(args)
+    if args.nvram_file == None:
+        env = []
+    else:
+        with open(args.nvram_file, "rb") as nv:
+            env = pickle.load(nv)
 
-    ql = Qiling([args.target], ".", env = env)
+    if args.extra_modules == None:
+        args.extra_modules = []
+
+    ql = Qiling(args.extra_modules + [args.target], ".", env = env, verbose=QL_VERBOSE.DEFAULT)
     ql.run()
 
 
@@ -21,8 +29,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("command", help="프로그램 실행 모드 선택", choices=["run", "fuzz"])
     parser.add_argument("target")
+    parser.add_argument("-x", "--extra-modules", help="Extra modules to load", nargs='+')
     parser.add_argument("-v", "--nvram-file", help="Pickled dictionary containing the NVRAM environment variables")
-    parser.add_argument("-o", "--output", help="Trace execution for debugging purposes", choices=["default", "disabled", "debug", "disasm", "dump"])
+    parser.add_argument("-o", "--verbose", help="Trace execution for debugging purposes", choices=["QL_VERBOSE.DEFAULT", "QL_VERBOSE.DEBUG", "QL_VERBOSE.DISASM", "QL_VERBOSE.OFF", "QL_VERBOSE.DUMP"])
 
     subparsers = parser.add_subparsers(help="Fuzzing modes", dest="mode")
     nvram_subparsers = subparsers.add_parser("nvram")
