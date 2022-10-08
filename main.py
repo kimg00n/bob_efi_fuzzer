1748e2da2794bde59996fe86c122dbeeed0b5adc
kimg00n <kevin1226716@gmail.com>
2022-10-08 11:20:18 +0900

    enable afl but doesn't work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import pefile
 import pickle
 from qiling import *
 from qiling.os.uefi.const import *
@@ -8,6 +9,7 @@ from qiling.const import QL_VERBOSE
 from qiling.extensions.sanitizers.heap import QlSanitizedMemoryHeap
 from qiling.extensions import trace
 from qiling.debugger.qdb import QlQdb
+from qiling.extensions import afl
 
 def my_abort(msg):
     print(f"\n*** {msg} ***\n")
@@ -27,6 +29,30 @@ def enable_sanitized_heap(ql, fault_rate=0):
     ql.os.heap = heap
     ql.loader.dxe_context.heap = heap
 
+def start_afl(ql: Qiling, user_data):
+    """Have Unicorn fork and start instrumentation.
+    """
+    (varname, infile) = user_data
+    def place_input_callback_nvram(ql: Qiling, input: bytes, _):
+        """
+        Injects the mutated variable to the emulated NVRAM environment.
+        """
+        ql.env[varname] = input
+
+    def validate_crash(err):
+        if not ql.os.heap.validate():
+            print(err)
+            my_abort("Canary corruption detected")
+    
+    place_input_callback = place_input_callback_nvram
+    if not afl.ql_afl_fuzz(ql,
+        input_file=infile, 
+        place_input_callback=place_input_callback, 
+        exits=[ql.os.exit_point], 
+        always_validate=True, 
+        validate_crash_callback=validate_crash):
+        os._exit(0)  # that's a looot faster than tidying up.
+
 def run(args):
     if args.nvram_file == None:
         env = []
@@ -41,14 +67,13 @@ def run(args):
     trace.enable_full_trace(ql)
     if args.sanitize == "y":
         enable_sanitized_heap(ql)
-    if args.gdb != None:
+    if args.gdb == "y":
         ql.debugger=True
     ql.run()
     if not ql.os.heap.validate():
         my_abort("Canary corruption detected")
 
 def fuzz(args):
-    print(args)
     if args.nvram_file == None:
         env = []
     else:
@@ -58,13 +83,17 @@ def fuzz(args):
     if args.extra_modules == None:
         args.extra_modules = []
 
-    ql = Qiling(args.extra_modules + [args.target], ".", env = env, verbose=QL_VERBOSE.DEBUG)
+    ql = Qiling(args.extra_modules + [args.target], ".", env = env, verbose=QL_VERBOSE.OFF)
     enable_sanitized_heap(ql)
-    if args.gdb != None:
-        ql.debugger=True
+
+    target = ql.loader.images[-1].path
+    pe = pefile.PE(target, fast_load=True)
+    image_base = ql.loader.images[-1].base
+    entry_point = image_base + pe.OPTIONAL_HEADER.AddressOfEntryPoint
+
+    # We want AFL's forkserver to spawn new copies starting from the main module's entrypoint.
+    ql.hook_address(callback=start_afl, address=entry_point, user_data=(args.varname, args.input))
     ql.run()
-    if not ql.os.heap.validate():
-        my_abort("Canary corruption detected")
 
 
 def main(args):
@@ -82,7 +111,7 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--nvram-file", help="Pickled dictionary containing the NVRAM environment variables")
     parser.add_argument("-o", "--verbose", help="Trace execution for debugging purposes", choices=["QL_VERBOSE.DEFAULT", "QL_VERBOSE.DEBUG", "QL_VERBOSE.DISASM", "QL_VERBOSE.OFF", "QL_VERBOSE.DUMP"], default="QL_VERBOSE.DEFAULT")
     parser.add_argument("-s", "--sanitize", help="Enable heap sanitizer", choices=["y", "n"], default="y")
-    parser.add_argument("-g", "--gdb", help="Enable gdb server", action="store_true")
+    parser.add_argument("-g", "--gdb", help="Enable gdb server at run mode", default="n")
 
     subparsers = parser.add_subparsers(help="Fuzzing modes", dest="mode")
     nvram_subparsers = subparsers.add_parser("nvram")
