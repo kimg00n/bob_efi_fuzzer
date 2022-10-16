bda46a4a852e622261fb14a6042ea5c64bc194a8
kimg00n <kevin1226716@gmail.com>
2022-10-16 14:39:02 +0900

    check crash

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # BoB_EFI_Fuzzer
 
 - EFI NVRAM Fuzzer by Best of the Best 11th.
-- Inspired by efi_fuzz from Sentinel-One(https://github.com/Sentinel-One/efi_fuzz)
+- Improved qiling and AFL++ version NVRAM fuzzing in [efi_fuzz](https://github.com/Sentinel-One/efi_fuzz) from Sentinel-One
+- It is compatible with the latest version of [qiling](https://github.com/qilingframework/qiling) & [AFL++](https://github.com/AFLplusplus/AFLplusplus)(qiling=1.4.4, AFL++=4.03c)
+
+## Install AFL++ & unicornafl
+  - install AFL++
+  ```
+  git clone https://github.com/AFLplusplus/AFLplusplus
+  cd AFLplusplus
+  make distrib
+  sudo make install
+  ```
+
+  - install unicornafl
+  ```
+  git submodule update --init --recursive
+  cd bindings/python/
+  python3 -m pip install -e .
+  ```
+
+  - You have to change unicornafl.py
+  ```
+  err = _uc2afl.uc_afl_fuzz(uc._uch, input_file.encode("utf-8"), cb1, ctypes.cast(
+      exits_array, ctypes.c_void_p), exits_len, cb2, always_validate, persistent_iters, ctypes.cast(idx, ctypes.c_void_p))
+
+  if err != UC_AFL_RET_OK:
+      del _data_dict[idx]
+      raise UcAflError(err)
+  ```
+  to
+  ```
+  err = _uc2afl.uc_afl_fuzz(uc._uch, input_file.encode("utf-8"), cb1, ctypes.cast(
+      exits_array, ctypes.c_void_p), exits_len, cb2, always_validate, persistent_iters, ctypes.cast(idx, ctypes.c_void_p))
+
+  if err == UC_AFL_RET_NO_AFL:
+      return False
+
+  elif err != UC_AFL_RET_OK:
+      del _data_dict[idx]
+      raise UcAflError(err)
+  ```
 
 ## How to use
     afl-fuzz -D -i afl_inputs/{varname} -o afl_outputs -U -- python3 ./main.py fuzz {target} -v {nvram} nvram {varname} @@
--- core/FuzzingManager.py
+++ core/FuzzingManager.py
@@ -4,7 +4,6 @@ from unicornafl import *
 import pefile
 from qiling import Qiling
 import os
-from qiling.extensions import afl
 
 def verbose_abort(ql):
     ql.os.emu_error()
@@ -13,18 +12,16 @@ def verbose_abort(ql):
 def start_afl(_ql: Qiling, user_data):
     (varname, infile) = user_data
 
-    def place_input_callback(uc, _input, _, data):
-        _ql.env[varname] = input
+    def place_input_callback(uc, input_bytes, iters, data):
+        _ql.env[varname] = input_bytes
     
-    def validate_crash(uc, result, _input_bs, persistent_round, data):
+    def validate_crash(uc, err, input_bytes, iters, data):
         if hasattr(_ql.os.heap, "validate"):
             if not _ql.os.heap.validate():
                 verbose_abort(_ql)
                 return True
-        elif result == UC_AFL_RET_NO_AFL:
-            return False
-        
-        crash = result != UC_ERR_OK
+
+        crash = (_ql.internal_exception is not None) or (err != UC_ERR_OK)
         return crash
 
     try:
@@ -34,8 +31,8 @@ def start_afl(_ql: Qiling, user_data):
                             exits=[_ql.os.exit_point],
                             always_validate=True,
                             validate_crash_callback=validate_crash)
-        if not ret or ret == UC_AFL_RET_NO_AFL:
-                                print("Dry run completed successfully without AFL attached.")
+        if not ret:
+                                _ql.log.warning("Dry run completed successfully without AFL attached.")
                                 os._exit(0)  # that's a looot faster than tidying up.
     except unicornafl.UcAflError as ex:
         if ex.errno != unicornafl.UC_AFL_RET_CALLED_TWICE:
